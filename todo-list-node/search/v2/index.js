1003572b9530b63c08370bd8b6e25abce2fddb3c
elliisabeta <142780441+Ellisabeta@users.noreply.github.com>
2026-05-04 22:44:35 +0200

    fix: adjusted idor in search

--- todo-list-node/search/v2/index.js
+++ todo-list-node/search/v2/index.js
@@ -2,18 +2,18 @@ const db = require('../../fw/db');
 const escapeHtml = require('escape-html');
 
 async function search(req) {
-    if (req.query.userid === undefined || req.query.terms === undefined){
+    if (req.query.userid === undefined || req.query.terms === undefined) {
         return "Not enough information to search";
     }
 
-    let userid = req.query.userid;
+    let userid = req.session.userid;
     let terms = req.query.terms;
     let result = '';
 
     let stmt = await db.executeStatement("SELECT ID, title, state FROM tasks WHERE userID = ? AND title LIKE ?", [userid, `%${terms}%`]);
     if (stmt.length > 0) {
-        stmt.forEach(function(row) {
-            result += escapeHtml(row.title)+' ('+escapeHtml(row.state)+')<br />';
+        stmt.forEach(function (row) {
+            result += escapeHtml(row.title) + ' (' + escapeHtml(row.state) + ')<br />';
         });
     }
 
