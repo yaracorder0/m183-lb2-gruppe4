02bc63b8f51bfd26daaa98f924e93c15b8631fc6
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2026-05-04 23:57:30 +0200

    git add logger

---
+++ todo-list-node/fw/logger.js
@@ -0,0 +1,21 @@
+const winston = require('winston');
+const path = require('path');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.File({ filename: path.join(__dirname, '../logs/security.log') }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    ],
+});
+
+module.exports = logger;
